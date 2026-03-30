9b2e5dab32031cc5ae939600987b63aa4f9f73aa
Renato <sboy63622@gmail.com>
2026-03-30 18:09:59 -0300

    Criação da geração de números, letras e simbolos feita.

---
+++ app.js
@@ -0,0 +1,7 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste")
+})
